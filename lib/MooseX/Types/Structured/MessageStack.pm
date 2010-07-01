14375407d661e5efbef1c1d58395ab5174d57283
John Napiorkowski <johnn@John-Napiorkowski-MacBook-Pro.local>
2010-07-01 10:22:59 -0400

    new message stack code file and bust out test

---
+++ lib/MooseX/Types/Structured/MessageStack.pm
@@ -0,0 +1,40 @@
+package MooseX::Types::Structured::MessageStack;
+use Moose;
+
+has 'level' => (
+    traits => ['Counter'],
+    is => 'ro',
+    isa => 'Num',
+    required => 0,
+    default => 0,
+    handles => {
+        inc_level => 'inc',
+        dec_level => 'dec',
+    },
+);
+
+# :level, :message, :tc
+has 'messages' => (
+    traits => ['Array'],
+    is => 'ro',
+    isa => 'ArrayRef[HashRef]',
+    required => 1,
+    default => sub { [] },
+    handles => {
+        has_messages => 'count',
+        add_message => 'push',
+        all_messages => 'elements',
+    },
+);
+
+sub as_string {
+    my @messages = (shift)->all_messages;
+    my $message = join("", map { "\n". (" " x $_->{level}) ."[+] " . $_->{message} } reverse @messages);
+    return $message;
+}
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
