4220915ac0e6f23185627a95927231168bc57724
Karen Etheridge <ether@cpan.org>
2015-08-15 19:40:05 -0700

    increment $VERSION after 0.34 release

--- lib/MooseX/Types/Structured.pm
+++ lib/MooseX/Types/Structured.pm
@@ -1,7 +1,7 @@
 package MooseX::Types::Structured;
 # ABSTRACT: Structured Type Constraints for Moose
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 use 5.008;
 use Moose::Util::TypeConstraints 1.06 'find_type_constraint';
